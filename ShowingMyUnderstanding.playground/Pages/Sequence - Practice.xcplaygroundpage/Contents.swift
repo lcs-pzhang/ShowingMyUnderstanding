911a88f37d5589d3c50bf896a842fece284b9ca6
Zhang Phillip <pzhang@lcs.on.ca>
2017-11-23 10:47:56 -0500

    finished the first part

--- ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
@@ -37,7 +37,38 @@ canvas.drawShapesWithBorders = false
 // Thick lines
 canvas.defaultLineWidth = 5
 
+
 // Add your code below... remember to use comments to indicate your intent
+canvas.fillColor=Color.black
+canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
+
+canvas.fillColor=Color.init(hue: 59, saturation: 100, brightness: 95, alpha: 100)
+canvas.drawEllipse(centreX: 120, centreY: 190, width: 90, height: 90)
+
+canvas.defaultLineWidth=5
+canvas.lineColor=Color.init(hue: 59, saturation: 100, brightness: 95, alpha: 100)
+canvas.drawLine(fromX: 120, fromY: 150, toX: 120, toY: 50)
+
+canvas.fillColor=Color.blue
+canvas.drawEllipse(centreX: 210, centreY: 190, width: 90, height: 90)
+
+canvas.defaultLineWidth=5
+canvas.lineColor=Color.blue
+canvas.drawLine(fromX: 210, fromY: 150, toX: 210, toY: 50)
+
+canvas.fillColor=Color.init(hue: 0, saturation: 100, brightness: 95, alpha: 70)
+canvas.drawEllipse(centreX: 165, centreY: 190, width: 90, height: 90)
+
+canvas.defaultLineWidth=5
+canvas.lineColor=Color.init(hue: 0, saturation: 100, brightness: 95, alpha: 70)
+canvas.drawLine(fromX: 165, fromY: 145, toX: 165, toY: 50)
+
+canvas.fillColor=Color.init(hue: 111, saturation: 100, brightness: 95, alpha: 70)
+canvas.drawEllipse(centreX: 70, centreY: 190, width: 90, height: 90)
+
+canvas.defaultLineWidth=5
+canvas.lineColor=Color.init(hue: 111, saturation: 100, brightness: 95, alpha: 70)
+canvas.drawLine(fromX: 70, fromY: 145, toX: 70, toY: 50)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
