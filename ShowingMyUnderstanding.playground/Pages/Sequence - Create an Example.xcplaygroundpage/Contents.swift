a37cb4cb3bcc7b28f04e2cb4e90d51ccf843e5e4
Zhang Phillip <pzhang@lcs.on.ca>
2017-11-23 11:18:15 -0500

    finished the second part

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -25,6 +25,36 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
 
+canvas.fillColor=Color.init(hue: 110, saturation: 90, brightness: 40, alpha: 100)
+canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
+
+canvas.drawShapesWithBorders = false
+canvas.fillColor=Color.init(hue: 31, saturation: 100, brightness: 95, alpha: 90)
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 100, height: 100)
+
+canvas.fillColor=Color.init(hue: 270, saturation: 95, brightness: 95, alpha: 80)
+canvas.drawEllipse(centreX: 115, centreY: 100, width: 70, height: 70)
+
+canvas.fillColor=Color.init(hue: 0, saturation: 95, brightness: 95, alpha: 80)
+canvas.drawEllipse(centreX: 175, centreY: 95, width: 70, height: 70)
+
+canvas.fillColor=Color.init(hue: 65, saturation: 95, brightness: 95, alpha: 80)
+canvas.drawEllipse(centreX: 100, centreY: 140, width: 70, height: 70)
+
+canvas.fillColor=Color.init(hue: 155, saturation: 95, brightness: 95, alpha: 70)
+canvas.drawEllipse(centreX: 200, centreY: 140, width: 70, height: 70)
+
+canvas.fillColor=Color.init(hue: 215, saturation: 95, brightness: 95, alpha: 80)
+canvas.drawEllipse(centreX: 185, centreY: 185, width: 70, height: 70)
+
+canvas.fillColor=Color.init(hue: 320, saturation: 95, brightness: 95, alpha: 80)
+canvas.drawEllipse(centreX: 125, centreY: 180, width: 70, height: 70)
+
+canvas.fillColor=Color.init(hue: 0, saturation: 0, brightness: 100, alpha: 85)
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 70, height: 70)
+canvas.defaultLineWidth=5
+canvas.lineColor=Color.black
+canvas.drawLine(fromX: 150, fromY: 100, toX: 150, toY: 30)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
