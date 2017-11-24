5f987861951ae840da552ac2a8cd0bc82a4f69ca
Zhang Phillip <pzhang@lcs.on.ca>
2017-11-24 12:42:07 -0500

    finished the next part of the assignment

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  # Conditionals - Create an Example
  
  Create an example where some aspect of the resulting image depends on using a conditional statement to respond to random number generation.
-  
+ 
  ## Remember
  
  Commit your work as you make progress on this page.
@@ -19,7 +19,23 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this command and add your code below...
+// Generate a random number
+
+let number=random(from: 0, toButNotIncluding: 4)
+
+
+if number == 0 {
+    canvas.fillColor=Color.blue
+   
+} else if number == 1{
+    canvas.fillColor=Color.yellow
+    
+} else if number == 2{
+    canvas.fillColor=Color.init(hue: 270, saturation: 95, brightness: 95, alpha: 85)
+} else if number == 3{
+    canvas.fillColor=Color.init(hue: 0, saturation: 0, brightness: 95, alpha: 85)
+}
+ canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
 
 /*:
  ## Template code
