8c2c2e056c62f735b6f1d4db744fedb00b7432ec
Zhang Phillip <pzhang@lcs.on.ca>
2017-11-24 13:48:59 -0500

    finished the second last part of the assignment

--- ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -18,8 +18,26 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
+canvas.fillColor=Color.init(hue: 181, saturation: 95, brightness: 95, alpha: 70)
+canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
 
-// Replace this comment and add your code below...
+for x in stride(from: 0, to: 300, by: 6) {
+    canvas.defaultLineWidth=1
+    canvas.lineColor=Color.white
+
+canvas.drawLine(fromX: 0, fromY: 300, toX: x+10, toY: 0)
+    
+    canvas.drawLine(fromX: 300, fromY: 0, toX: x-5, toY: 300)
+}
+
+for x in stride(from: 0, to: 300, by: 6) {
+    canvas.defaultLineWidth=5
+    canvas.lineColor=Color.init(hue: 80, saturation: 95, brightness: 90, alpha: 60)
+    canvas.drawLine(fromX: 0, fromY: 0, toX: 300, toY: x+10)
+}
+
+
+
 
 /*:
  ## Template code
